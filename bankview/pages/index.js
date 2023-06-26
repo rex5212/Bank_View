d8b1e3ac9a10f26a79df5e8ee41dc168aca37543
Renato Ostemberg <xrexv158925lpo@gmail.com>
2023-06-26 17:55:46 -0300

    BankView

--- bankview/pages/index.js
+++ bankview/pages/index.js
@@ -20,49 +20,7 @@ const index = () => {
   // }
 
   return (
-    <BaseBar>
-      <Row md={3}>
-        <Col>
-          <p className='text-white px-4 pt-4'>dashdbsa vdsagdvasgdv vdasgdvas das dvasdvasd vhasg vdash dvas</p>
-          <BasicDiv>
-            <Table striped bordered hover className='table-transparent textW'>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-              </tbody>
-            </Table>
-          </BasicDiv>
-        </Col>
-        <Col>
-          <BasicDiv>
-
-          </BasicDiv>
-          <BasicDiv>
-
-          </BasicDiv>
-        </Col>
-        <Col>
-          <BasicDiv>
-
-          </BasicDiv>
-          <BasicDiv>
-
-          </BasicDiv>
-        </Col>
-      </Row>
-    </BaseBar>
+    <></>
   )
 }
 
--- bankview/pages/menu/index.js
+++ bankview/pages/menu/index.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import BaseBar from '../components/BaseBar'
+import BasicDiv from '../components/BasicDiv'
 import axios from "axios"
+import { Col, Row, Table } from 'react-bootstrap'
 
 export const index = () => {
 
@@ -9,13 +12,55 @@ export const index = () => {
     getAll()
   }, [])
 
-  function getAll(){
-    axios.get('/api/menu').then(resultado=>{
+  function getAll() {
+    axios.get('/api/menu').then(resultado => {
       setMenu(resultado.data)
     })
   }
 
   return (
-    <div>index</div>
+    <BaseBar>
+      <Row md={3}>
+        <Col>
+          <p className='text-white px-4 pt-4'>dashdbsa vdsagdvasgdv vdasgdvas das dvasdvasd vhasg vdash dvas</p>
+          <BasicDiv>
+            <Table striped bordered hover className='table-transparent textW'>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Username</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+              </tbody>
+            </Table>
+          </BasicDiv>
+        </Col>
+        <Col>
+          <BasicDiv>
+
+          </BasicDiv>
+          <BasicDiv>
+
+          </BasicDiv>
+        </Col>
+        <Col>
+          <BasicDiv>
+
+          </BasicDiv>
+          <BasicDiv>
+
+          </BasicDiv>
+        </Col>
+      </Row>
+    </BaseBar>
   )
 }
