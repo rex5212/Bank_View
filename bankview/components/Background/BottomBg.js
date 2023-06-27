1db6d96eff9e906006d939e0f673472f38d1ffaf
Renato Ostemberg <xrexv158925lpo@gmail.com>
2023-06-27 18:46:29 -0300

    Bank View

---
+++ bankview/components/Background/BottomBg.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BottomBg = () => {
+    return (
+        <svg width="588" height="366" viewBox="0 0 588 366" fill="none" xmlns="http://www.w3.org/2000/svg" className='position-fixed bottom-0 end-0'>
+            <path d="M437.115 366.179L588.78 271.883L588.78 366.179L437.115 366.179Z" fill="#000AFF" />
+            <path d="M437.115 366.179L588.78 271.883L588.78 366.179L437.115 366.179Z" fill="black" fill-opacity="0.4" />
+            <path d="M218.632 366.178L588.78 136.045L588.78 271.884L437.116 366.178L218.632 366.178Z" fill="#000AFF" />
+            <path d="M218.632 366.178L588.78 136.045L588.78 271.884L437.116 366.178L218.632 366.178Z" fill="black" fill-opacity="0.2" />
+            <path d="M0.146973 366.178L588.78 0.206045L588.78 136.045L218.631 366.178L0.146973 366.178Z" fill="#000AFF" />
+        </svg>
+    )
+}
+
+export default BottomBg
--- bankview/pages/index.js
+++ bankview/pages/index.js
@@ -3,15 +3,31 @@ import BaseBar from '../components/BaseBar'
 import BasicDiv from '../components/BasicDiv'
 import axios from "axios"
 import { Col, Row, Table } from 'react-bootstrap'
+import Bgv1 from '../components/Background/Bgv1'
+import BottomBg from '../components/Background/BottomBg'
+import TopBg from '../components/Background/TopBg'
+import Logo from '../components/Logo/Logo'
+import { useForm } from 'react-hook-form'
 
 
 const index = () => {
 
   // const [bolsa, setBolsa] = useState([])
+  const [isvisible, setIsvisible] = useState(true)
+  const { register, handleSubmit } = useForm()
 
   // useEffect(() => {
   //   getAll()
   // }, [])
+
+  function loga(dados) {
+
+  }
+
+  function visible() {
+    setIsvisible(!isvisible)
+  }
+
 
   // function getAll(){
   //   axios.get('/api/bolsa').then(resultado=>{
@@ -20,7 +36,43 @@ const index = () => {
   // }
 
   return (
-    <></>
+    <div className='d-flex align-items-center justify-content-center'>
+      <TopBg />
+      {isvisible == false ? (<Logo onClick={visible()} />)
+        : (
+          <div className='pt-5'>
+            <div class="rounded-xl bg-gray-800 bg-opacity-50 px-24 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+              <div class="text-white">
+                <div class="mb-8 flex flex-col items-center">
+                  <div className='py-10'>
+                    <Logo />
+                  </div>
+                  <h1 class="mb-2 text-5xl text-[#000AFF]">Bank View</h1>
+                </div>
+                <form action="#">
+                  <div class="mb-8 flex flex-col items-center">
+                    <span class="text-gray-300 text-center pb-2">Entre com seu E-mail</span>
+                    <div class="mb-14 text-lg">
+                      <input class="rounded-3xl border-none bg-[#000AFF] bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md" type="text" name="name" placeholder="id@email.com" {...register('name')} />
+                    </div>
+
+                    <div class="mb-14 text-lg">
+                      <input class="rounded-3xl border-none bg-[#000AFF] bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md" type="Password" name="name" placeholder="*********" {...register('dataBegin')} />
+                    </div>
+
+                    <div class="flex justify-center text-lg text-black">
+                      <button type="submit" class="rounded-3xl bg-[#000AFF] bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600" onClick={handleSubmit(loga)}>Login</button>
+                    </div>
+
+                    <a class="mt-4 text-lg text-center hover:text-yellow-600"><p>Não possui Login,</p><p class="text-[#000AFF] hover:text-yellow-600">Cadastre Aqui</p></a>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          )}
+      <BottomBg />
+    </div>
   )
 }
 
