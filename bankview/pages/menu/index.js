ab5ffb75a0ae30fbf734b2672dda4bc5e204d923
unknown <21114290027@academico.iesb.br>
2023-06-27 22:45:50 -0300

    Bank View

--- bankview/pages/menu/index.js
+++ bankview/pages/menu/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import BasicDiv from '../../components/BasicDiv' 
+import BasicDiv from '../../components/BasicDiv'
 import axios from "axios"
 import { Col, Row, Table } from 'react-bootstrap'
 import BaseBar from '../../components/BaseBar'
+import BasicTable from '../../components/Table/BasicTable'
 
 const index = () => {
 
@@ -24,150 +25,130 @@ const index = () => {
         <Col>
           <p className='text-white px-4 pt-4'>dashdbsa vdsagdvasgdv vdasgdvas das dvasdvasd vhasg vdash dvas</p>
           <BasicDiv>
-            <Table striped bordered hover className='table-transparent textW'>
-              <thead>
-                <tr>
-                  <th>...</th>
-                  <th>...</th>
-                  <th>...</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                </tr>
-              </tbody>
-            </Table>
+            <thead>
+              <tr>
+                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase opacity-100	">
+                  Category
+                </th>
+                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase opacity-100	">
+                  Price
+                </th>
+                <th scope="col" class="p-4">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y opacity-100">
+              <tr class="hover:bg[#000AFF]">
+                <td class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">Desktop PC</td>
+                <td class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">$1999</td>
+                <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                  <a href="#" class="text-[#000AFF] hover:underline">Edit</a>
+                </td>
+              </tr>
+            </tbody>
           </BasicDiv>
         </Col>
         <Col>
           <BasicDiv>
-          <Table striped bordered hover className='table-transparent textW'>
-              <thead>
-                <tr>
-                  <th>...</th>
-                  <th>...</th>
-                  <th>...</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                </tr>
-              </tbody>
-            </Table>
+            <thead>
+              <tr>
+                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase opacity-100	">
+                  Category
+                </th>
+                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase opacity-100	">
+                  Price
+                </th>
+                <th scope="col" class="p-4">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y opacity-100">
+              <tr class="hover:bg[#000AFF]">
+                <td class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">Desktop PC</td>
+                <td class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">$1999</td>
+                <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                  <a href="#" class="text-[#000AFF] hover:underline">Edit</a>
+                </td>
+              </tr>
+            </tbody>
           </BasicDiv>
           <BasicDiv>
-          <Table striped bordered hover className='table-transparent textW'>
-              <thead>
-                <tr>
-                  <th>...</th>
-                  <th>...</th>
-                  <th>...</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                </tr>
-              </tbody>
-            </Table>
+            <thead>
+              <tr>
+                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase opacity-100	">
+                  Category
+                </th>
+                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase opacity-100	">
+                  Price
+                </th>
+                <th scope="col" class="p-4">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y opacity-100">
+              <tr class="hover:bg[#000AFF]">
+                <td class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">Desktop PC</td>
+                <td class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">$1999</td>
+                <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                  <a href="#" class="text-[#000AFF] hover:underline">Edit</a>
+                </td>
+              </tr>
+            </tbody>
           </BasicDiv>
         </Col>
         <Col>
           <BasicDiv>
-          <Table striped bordered hover className='table-transparent textW'>
-              <thead>
-                <tr>
-                  <th>...</th>
-                  <th>...</th>
-                  <th>...</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                </tr>
-              </tbody>
-            </Table>
+            <thead>
+              <tr>
+                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase opacity-100	">
+                  Category
+                </th>
+                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase opacity-100	">
+                  Price
+                </th>
+                <th scope="col" class="p-4">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y opacity-100">
+              <tr class="hover:bg[#000AFF]">
+                <td class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">Desktop PC</td>
+                <td class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">$1999</td>
+                <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                  <a href="#" class="text-[#000AFF] hover:underline">Edit</a>
+                </td>
+              </tr>
+            </tbody>
           </BasicDiv>
           <BasicDiv>
-          <div class="flex flex-col">
-    <div class="overflow-x-auto shadow-md sm:rounded-lg">
-        <div class="inline-block min-w-full align-middle">
-            <div class="overflow-hidden ">
-                <table class="min-w-full divide-y opacity-100	 table-fixed ">
-                    <thead class="bg[#000AFF] dark:bg-gray-700">
-                        <tr>
-                            <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase opacity-100	">
-                                Product Name
-                            </th>
-                            <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase opacity-100	">
-                                Category
-                            </th>
-                            <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase opacity-100	">
-                                Price
-                            </th>
-                            <th scope="col" class="p-4">
-                                <span class="sr-only">Edit</span>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody class="bg-white divide-y opacity-100">
-                        <tr class="hover:bg[#000AFF]">
-                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">Apple Imac 27"</td>
-                            <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">Desktop PC</td>
-                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">$1999</td>
-                            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-                                <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            </td>
-                        </tr>
-                        <tr class="hover:bg[#000AFF] ">
-                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">Apple MacBook Pro 17"</td>
-                            <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">Laptop</td>
-                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">$2999</td>
-                            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-                                <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            </td>
-                        </tr>
-                        <tr class="hover:bg[#000AFF] ">
-                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">iPhone 13 Pro</td>
-                            <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">Phone</td>
-                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">$999</td>
-                            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-                                <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            </td>
-                        </tr>
-                        <tr class="hover:bg[#000AFF] ">
-                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">Apple Magic Mouse 2</td>
-                            <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">Accessories</td>
-                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">$99</td>
-                            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-                                <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            </td>
-                        </tr>
-                        <tr class="hover:bg[#000AFF] ">
-                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">Apple Watch Series 7</td>
-                            <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">Accessories</td>
-                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">$599</td>
-                            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-                                <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-    </div>
+            <BasicTable>
+              <thead>
+                <tr>
+                  <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase opacity-100	">
+                    Category
+                  </th>
+                  <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase opacity-100	">
+                    Price
+                  </th>
+                  <th scope="col" class="p-4">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="divide-y opacity-100">
+                <tr class="hover:bg[#000AFF]">
+                  <td class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">Desktop PC</td>
+                  <td class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">$1999</td>
+                  <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                    <a href="#" class="text-[#000AFF] hover:underline">Edit</a>
+                  </td>
+                </tr>
+              </tbody>
+            </BasicTable>
           </BasicDiv>
         </Col>
       </Row>
