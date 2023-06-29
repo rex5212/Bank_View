0f0d562000c2472ed57cabe43d09052f12c773e6
unknown <21114290027@academico.iesb.br>
2023-06-28 22:05:35 -0300

    Bank View

--- bankview/pages/menu/index.js
+++ bankview/pages/menu/index.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import BasicDiv from '../../components/BasicDiv'
 import axios from "axios"
-import { Col, Row, Table } from 'react-bootstrap'
+import { Button, Col, Offcanvas, Row, Table } from 'react-bootstrap'
 import BaseBar from '../../components/BaseBar'
 import BasicTable from '../../components/Table/BasicTable'
 import Maintenance from '../../components/Maintenance/Maintenance'
+import BaseBg from '../../components/BaseBg'
+
 
 const index = () => {
 
   const [menu, setMenu] = useState([])
+  const [modal, setModal] = useState(false)
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
 
   useEffect(() => {
     getAll()
@@ -48,10 +57,21 @@ const index = () => {
                 </tr>
               </thead>
               <tbody class="divide-y opacity-100 border-[#000AFF]">
-                <Maintenance/>
+                <Maintenance />
               </tbody>
             </BasicTable>
           </BasicDiv>
+          <button class='text-white' onClick={() => showModal()}>Modal</button>
+          {modal == true ? (
+            <>
+              <div class="py-12 bg-opacity-5 bg-gray-250 shadow-lg backdrop-blur-md transition duration-500 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" onClick={() => (showModal())}>
+                <div role="alert" class=" bg-opacity-50 px-24 py-10 shadow-lg backdrop-blur-md max-sm:px-8 flex items-center justify-center">
+                  <div class="relative text-white py-8 px-5 md:px-10 bg-black rounded-xl border-t-2 border-b-2 border-[#000AFF]">
+                    <p>Teste de Modal</p>
+                  </div>
+                </div>
+              </div>
+            </>) : ''}
         </Col>
         <Col>
           <BasicDiv>
@@ -67,7 +87,7 @@ const index = () => {
                 </tr>
               </thead>
               <tbody class="divide-y opacity-100 border-[#000AFF]">
-                <Maintenance/>
+                <Maintenance />
               </tbody>
             </BasicTable>
           </BasicDiv>
@@ -84,7 +104,7 @@ const index = () => {
                 </tr>
               </thead>
               <tbody class="divide-y opacity-100 border-[#000AFF]">
-                <Maintenance/>
+                <Maintenance />
               </tbody>
             </BasicTable>
           </BasicDiv>
@@ -103,7 +123,7 @@ const index = () => {
                 </tr>
               </thead>
               <tbody class="divide-y opacity-100 border-[#000AFF]" className='scrollbar scrollbar-primary'>
-                <Maintenance/>
+                <Maintenance />
               </tbody>
             </BasicTable>
           </BasicDiv>
@@ -120,12 +140,30 @@ const index = () => {
                 </tr>
               </thead>
               <tbody class="divide-y opacity-100 border-[#000AFF]">
-                <Maintenance/>
+                <Maintenance />
               </tbody>
             </BasicTable>
           </BasicDiv>
         </Col>
       </Row>
+
+      <Button variant="primary" onClick={handleShow}>
+        Launch
+      </Button>
+
+      <Offcanvas show={show} onHide={handleClose} placement='end'>
+
+
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            Some text as placeholder. In real life you can have the elements you
+            have chosen. Like, text, images, lists, etc.
+          </Offcanvas.Body>
+
+          
+      </Offcanvas> 
     </BaseBar>
   )
 }
